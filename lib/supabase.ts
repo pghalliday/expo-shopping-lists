e8bc7f2ac848f9752762739443e99ee49b95713e
Peter Halliday <pghalliday@gmail.com>
2025-03-18 16:27:53 +0000

    setup supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,36 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import {createClient} from '@supabase/supabase-js'
+import {SyncDatabaseChangeSet, synchronize} from "@nozbe/watermelondb/sync";
+import {database} from "~/model/database";
+
+const supabaseUrl = process.env.SUPABASE_URL!
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+})
+
+export async function sync() {
+    await synchronize({
+        database,
+        pullChanges: async ({lastPulledAt, schemaVersion, migration}) => {
+            const {data, error} = await supabase.rpc('pull', {
+                last_pulled_at: lastPulledAt,
+            })
+            const {changes, timestamp} = data as {
+                changes: SyncDatabaseChangeSet
+                timestamp: number
+            }
+            return {changes, timestamp}
+        },
+        pushChanges: async ({changes, lastPulledAt}) => {
+            const {error} = await supabase.rpc('push', {changes})
+        },
+        sendCreatedAsUpdated: true,
+    })
+}
