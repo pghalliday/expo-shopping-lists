42d84730f3f8b636d13624409180f10b6da86b37
Peter Halliday <pghalliday@gmail.com>
2025-06-17 17:36:35 +0200

    support web

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-    plugins: [['@babel/plugin-proposal-decorators', { 'legacy': true }]],
-  };
+    api.cache(true);
+    return {
+        presets: [['babel-preset-expo', {jsxImportSource: 'nativewind'}], 'nativewind/babel'],
+        plugins: [
+            ['@babel/plugin-proposal-decorators', {'legacy': true}],
+            ['@babel/plugin-transform-flow-strip-types'],
+            ['@babel/plugin-proposal-class-properties', {loose: true}],
+        ],
+    };
 };
